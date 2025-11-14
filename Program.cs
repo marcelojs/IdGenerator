bae5a66c6c17504c42b9db69423a93ed7f390c9e
Marcelo José <marcelojs90@gmail.com>
2025-11-14 09:00:18 -0300

    juntando tuido

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Numerics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace id_generator
 {
@@ -7,14 +8,83 @@ namespace id_generator
         public static void Main(string[] args)
         {
             var generator = new Generator();
+            var resultFinal = generator.GerarChave();
+        }
+    }
 
-            generator.GerarChave();
-            var tick2 = generator.TickDataMeio;
-            var result2 = RefinarParteFinalChave2(tick2);
-            Console.WriteLine(result2);
+    public class Generator
+    {
+        public long TickDateInicio { get; private set; }
+        public long TickDataMeio { get; private set; }
+        public long TickDataAtual { get; private set; }
+        public string? TickFinalProcessado { get; private set; }
+        public DateTime DataInicio { get; private set; }
+
+        public long GerarChave()
+        {
+            var data3 = DateTime.Now;
+            data3 = data3.Date;
+            var anoDestino = data3.Year - 1000;
+
+            DataInicio = ValidarDataDeAnoBissexto(data3, anoDestino);
+
+            var timeResult = data3 - DataInicio;
+
+            var apoiadorRandomico = new Random();
+
+            var fator = apoiadorRandomico.NextDouble();
+            var data2 = DataInicio.AddTicks((long)(timeResult.Ticks * fator));
+
+            var potencia = apoiadorRandomico.Next(5, 10);
+            var resultPotencia = (long)Math.Pow(data3.Day, potencia);
+
+            var tickData2Final = data2.Ticks + resultPotencia;
+            var data2Final = new DateTime(tickData2Final, DateTimeKind.Local);
+
+            TickDateInicio = DataInicio.Ticks;
+            TickDataMeio = data2Final.Ticks;
+            TickDataAtual = data3.Ticks;
+
+            if (TickDataMeio < TickDateInicio || TickDataMeio > TickDataAtual)
+            {
+                Console.WriteLine("Data do meio fora do intervalo, será reajustada por data pivo");
+                TickDataMeio = AjusteTickPorDataPivo(data2Final);
+            }
+
+            return TickDataMeio;
         }
 
-        public static long RefinarParteFinalChave2(long tickFinal)
+        public DateTime ValidarDataDeAnoBissexto(DateTime dataEmAvaliacao, int anoDestino)
+        {
+            var isBisexto = DateTime.IsLeapYear(anoDestino);
+
+            if (dataEmAvaliacao.Month == 2 && dataEmAvaliacao.Day == 29)
+            {
+                // Se o ano destino for bissexto, mantém 29/02
+                if (isBisexto)
+                    DataInicio = new DateTime(anoDestino, 2, 29);
+                else
+                    DataInicio = new DateTime(anoDestino, 2, 28); // fallback seguro
+            }
+            else
+                DataInicio = new DateTime(anoDestino, dataEmAvaliacao.Month, dataEmAvaliacao.Day);
+
+            return DataInicio;
+        }
+
+        public long AjusteTickPorDataPivo(DateTime dataMeio)
+        {
+            var dataPivo = DateTime.Now;
+            var anoDestino = dataPivo.Year - 500;
+
+            dataPivo = ValidarDataDeAnoBissexto(dataMeio, anoDestino);
+
+            var tickFinal = RefinarParteFinalChave2(dataPivo.Ticks); 
+
+            return tickFinal;
+        }
+
+        public long RefinarParteFinalChave2(long tickFinal)
         {
             var randFator = new Random();
 
@@ -37,78 +107,6 @@ namespace id_generator
             return tickRefinado;
         }
 
-
-    }
-
-    public class Generator
-    {
-        public long TickDateInicio { get; private set; }
-        public long TickDataMeio { get; private set; }
-        public long TickDataAtual { get; private set; }
-        public string? TickFinalProcessado { get; private set; }
-        public DateTime DataInicio { get; private set; }
-
-        public void GerarChave()
-        {
-            var data3 = DateTime.Now;
-            data3 = data3.Date;
-            var anoDestino = data3.Year - 1000;
-            var isBisexto = DateTime.IsLeapYear(anoDestino);
-
-            // Verifica se a data original é 29/02
-            if (data3.Month == 2 && data3.Day == 29)
-            {
-                // Se o ano destino for bissexto, mantém 29/02
-                if (isBisexto)
-                    DataInicio = new DateTime(anoDestino, 2, 29);
-                else
-                    DataInicio = new DateTime(anoDestino, 2, 28); // fallback seguro
-            }
-            else
-                DataInicio = new DateTime(anoDestino, data3.Month, data3.Day);
-
-            var timeResult = data3 - DataInicio;
-
-            var fator = new Random().NextDouble();
-            var data2 = DataInicio.AddTicks((long)(timeResult.Ticks * fator));
-
-            var randPotencia = new Random();
-
-            var potencia = randPotencia.Next(5, 10);
-            var resultPotencia = (long)Math.Pow(data3.Day, potencia);
-
-            var tickData2Final = data2.Ticks + resultPotencia;
-            var data2Final = new DateTime(tickData2Final, DateTimeKind.Local);
-
-            TickDateInicio = DataInicio.Ticks;
-            TickDataMeio = data2Final.Ticks;
-            TickDataAtual = data3.Ticks;
-        }
-
-        public static long AjusteTickPorDataPivo(DateTime dataMeio)
-        {
-            long tick = 0;
-            var dataPivo = DateTime.Now;
-            var anoDestino = dataPivo.Year - 500;
-            var isBisexto = DateTime.IsLeapYear(anoDestino);
-
-            if (dataPivo.Month == 2 && dataPivo.Day == 29)
-            {
-                // Se o ano destino for bissexto, mantém 29/02
-                if (isBisexto)
-                    dataPivo = new DateTime(anoDestino, 2, 29);
-                else
-                    dataPivo = new DateTime(anoDestino, 2, 28); 
-            }
-            else
-                dataPivo = new DateTime(anoDestino, dataPivo.Month, dataPivo.Day);
-
-
-
-            return tick;
-        }
-        
-        
     }
 
     public class InstanceManager
